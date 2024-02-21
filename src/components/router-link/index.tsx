deef0018a08bf54f330490cffb6ccfd613ea9dfa
Acadbek <ibnnumon@gmail.com>
2024-02-22 03:43:53 +0500

    [FEAT]: active link

--- src/components/router-link/index.tsx
+++ src/components/router-link/index.tsx
@@ -1,11 +1,15 @@
 import { LinkTypes } from "@/types"
 import { NavLink } from "react-router-dom"
 
-export const Link = ({ path, icon, title }: LinkTypes) => {
+export const Link = ({ path, icon, title, className }: LinkTypes) => {
   return (
-    <NavLink to={path} className="y-center border py-[13px] px-4 gap-4">
-      <img src={icon} alt="" />
-      <span className="text-sm font-medium leading-[22px] tracking-[0.2] text-left">{title}</span>
-    </NavLink>
+    <NavLink to={path}>
+      {({ isActive }) => (
+        <div style={{ backgroundColor: isActive ? '#edf2f7' : '' }} className="group y-center mb-1 hover:bg-[#edf2f7] rounded-xl py-[13px] px-4 gap-4" {...className}>
+          <img className="sidebar-icon" src={icon?.props?.src} alt="Sidebar Icon" />
+          <span className="text-sm font-medium capitalize leading-[22px] tracking-[0.2] text-left">{title}</span>
+        </div>
+      )}
+    </NavLink >
   )
 }
