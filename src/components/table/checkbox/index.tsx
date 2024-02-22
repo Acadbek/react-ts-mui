8eeb4358839aa9c38f1997232242fca88a600f1b
Acadbek <ibnnumon@gmail.com>
2024-02-22 22:44:14 +0500

    [FEAT]: Add checkbox component & refactor teble

---
+++ src/components/table/checkbox/index.tsx
@@ -0,0 +1,10 @@
+const Checkbox = () => {
+  return (
+    <div className="flex items-center">
+      <input id="checkbox-table-search-1" type="checkbox" className="w-5 h-5 cursor-pointer" />
+      <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+    </div>
+  )
+}
+
+export default Checkbox;
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 import { useQuery } from '@tanstack/react-query';
 import { productApi } from '@/api/products';
+import Checkbox from '@/components/table/checkbox';
 
 export const Products = () => {
   const { data, isLoading, isError } = useQuery(
@@ -12,25 +13,26 @@ export const Products = () => {
 
   console.log(data);
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (isError) return <div>Error fetching products</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching products</div>;
 
   return (
     <>
       <Helmet>
         <title>Projects</title>
       </Helmet>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+      <div className="relative mt-8 p-6 overflow-x-auto shadow-[10px_10px_50px_0px_#64748B0A] sm:rounded-lg bg-white">
+        <table className="w-full border-spacing-6 text-sm text-left rtl:text-right text-gray-700 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr className='bg-[#edf2f7] h-16 box-border leading-6 tracking-[0.2px] text-left'>
               <th scope="col" className="p-4">
                 <div className="flex items-center">
-                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-offset-gray-100 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                   <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
                 </div>
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 ">
                 Product name
               </th>
               <th scope="col" className="px-6 py-3">
@@ -48,62 +50,77 @@ export const Products = () => {
               <th scope="col" className="px-6 py-3">
                 Status
               </th>
+              <th scope="col" className="px-6 py-3">
+                <button>
+                  <img src="src/assets/icons/dots.svg" alt="" />
+                </button>
+              </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className='text-base text-[#0F172A] font-semibold leading-6 tracking-[0.20px] text-left'>
             {data?.map(item => (
-              <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <tr key={item?.id} className="bg-white h-16 border-b border-[#F1F5F9]">
                 <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                  </div>
+                  <Checkbox />
                 </td>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {item.title}
+                <th scope="row" className="flex items-center pl-2 gap-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                  <img className='w-12 h-12' src={item.productImages[0] || 'https://picsum.photos/seed/picsum/200/300'} alt="" />
+                  {item?.title}
                 </th>
                 <td className="px-6 py-4">
                   {item?.brand?.title}
                 </td>
                 <td className="px-6 py-4">
-                  {item.stock}
+                  {item?.stock}
                 </td>
                 <td className="px-6 py-4">
-                  {item.sales}
+                  {item?.sales}
                 </td>
                 <td className="px-6 py-4">
-                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                  {item?.productPricings?.price}
+                </td>
+                <td className="px-6 py-4">
+                  <label className="inline-flex items-center cursor-pointer">
+                    <input type="checkbox" defaultChecked={item?.status} value="" className="sr-only peer" />
+                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-[#cbd5e1] peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#2563eb]"></div>
+                  </label>
+                </td>
+                <td className="px-6 py-4">
+                  <a href="#" className="font-medium text-blue-600 hover:underline">
+                    <img src="/src/assets/icons/dots.svg" alt="" />
+                  </a>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
         <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
-          <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span className="font-semibold text-gray-900 dark:text-white">1000</span></span>
+          <span className="text-sm font-normal text-gray-500 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span className="font-semibold text-gray-900">1-10</span> of <span className="font-semibold text-gray-900">1000</span></span>
           <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">Previous</a>
             </li>
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">1</a>
             </li>
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">2</a>
             </li>
             <li>
-              <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+              <a href="#" aria-current="page" className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">3</a>
             </li>
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">4</a>
             </li>
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">5</a>
             </li>
             <li>
-              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+              <a href="#" className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">Next</a>
             </li>
           </ul>
         </nav>
+
       </div>
 
     </>
