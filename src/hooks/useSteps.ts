1da6acd20b8e69ad9906109a2ab0b8be80aec260
Acadbek <ibnnumon@gmail.com>
2024-02-23 11:38:36 +0500

    [FEAT]: useStep hook

---
+++ src/hooks/useSteps.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+interface StepTypes {
+  currentStep: number;
+  next: () => void;
+  prev: () => void;
+  goToStep: (stepNumber: number) => void;
+  isFirstStep: boolean;
+  isLastStep: boolean;
+}
+
+const useSteps = (totalSteps: number): StepTypes => {
+  const [currentStep, setCurrentStep] = useState<number>(1);
+
+  const next = (): void => {
+    setCurrentStep((prevStep: number) => Math.min(prevStep + 1, totalSteps));
+  };
+
+  const prev = (): void => {
+    setCurrentStep((prevStep: number) => Math.max(prevStep - 1, 1));
+  };
+
+  const goToStep = (stepNumber: number): void => {
+    setCurrentStep(Math.max(1, Math.min(stepNumber, totalSteps)));
+  };
+
+  const isFirstStep: boolean = currentStep === 1;
+  const isLastStep: boolean = currentStep === totalSteps;
+
+  return {
+    next,
+    prev,
+    currentStep,
+    goToStep,
+    isFirstStep,
+    isLastStep
+  };
+};
+
+export default useSteps;
