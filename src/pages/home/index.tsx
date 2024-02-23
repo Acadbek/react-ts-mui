298c3ead93c3173b378994519a70012bc176175a
Acadbek <ibnnumon@gmail.com>
2024-02-23 11:38:07 +0500

    [FEAT]: modal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,13 @@
 import Navbar from '@/components/UI/navbar';
 import { Button } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
   return (
     <>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
       <Navbar />
       <Button variant="contained">Text</Button>
     </>
